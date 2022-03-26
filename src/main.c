a257a778c0817757819d84d97e71f323bb298c9b
Brian Aung <soekhantaung16@gmail.com>
2022-03-26 23:51:05 +1100

    implement task 3 (sloppy and messy rn)

--- src/main.c
+++ src/main.c
@@ -32,38 +32,63 @@ int main(int argc, char **argv) {
     /* TASK 1 */
     int num_files = 0;
     for (int i=0; i<num_processes; i++) {
+        /* there will always be at least 2 files in one process */
         if (i == 0) {
             num_files = 2;
             continue;
         }
-        if (check_files(processes, num_processes, i, processes[i].locked_file_id, 2, NULL)) {
+
+        /* it is a new file if it doesn't match with files from previous processes */
+        if (check_files(processes, num_processes, i, processes[i].locked_file_id, 2, NULL) == -1) {
             num_files++;
         }
-        if (check_files(processes, num_processes, i, processes[i].requested_file_id, 2, NULL)) {
+        if (check_files(processes, num_processes, i, processes[i].requested_file_id, 2, NULL) == -1) {
             num_files++;
         }
+        /***************************************************************************/
     }
     printf("Processes %d\nFiles %d\n", num_processes, num_files);
+
 
     /* TASK 2 */
     int highest_occurrence = 0;
     if (compute_time) {
-        // do task 2
-        int k = 1;
         int execution_time = 0;
 
         if (num_processes) {
             execution_time += 1;
             
             for (int i=0; i<num_processes; i++) {
-                int occurrence = 1;
+                int count = 1;
+                /* if the file is not the new file */
                 if (check_files(processes, num_processes, i, 
-                            processes[i].requested_file_id, 1, &occurrence)) {
-                    if (occurrence > highest_occurrence) { highest_occurrence = occurrence; }
+                            processes[i].requested_file_id, 1, &count) != -1) {
+                    /* then check if it has the highest occurrence */
+                    if (count > highest_occurrence) { highest_occurrence = count; }
                 }
             }
         }
+        /* execution time = largest k (no of processes requesting the same file) + 2 */
         printf("Execution time %d\n", execution_time + highest_occurrence);
+    }
+
+    /* TASK 3 */
+    int to_break[num_processes];
+    int to_break_count = 0;
+    int tmp = 0;
+    for (int i=0; i<num_processes; i++) {
+        tmp = check_files(processes, num_processes, i, processes[i].requested_file_id, 0, NULL); 
+        if (tmp != -1) { to_break[to_break_count] = tmp; to_break_count++; }
+    }
+
+    if (to_break_count) {
+        printf("Deadlock Detected\nTerminate ");
+        for (int i=0; i<to_break_count; i++) {
+            printf("%d ", to_break[i]);
+        }
+        printf("\n");
+    } else {
+        printf("No deadlocks\n");
     }
 
     return 0;
