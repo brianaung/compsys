9b24416b00f7a6703927fc3d0430b9d40e0e17b9
Brian Aung <soekhantaung16@gmail.com>
2022-03-26 23:50:22 +1100

    modify check_file function to return deadlocked process id

--- src/utils.c
+++ src/utils.c
@@ -31,48 +31,53 @@ void save_processes(char* input_file, struct process** p, int* num_processes) {
     *p = processes;
 }
 
-int check_files(struct process* processes, int num_process, int curr_process, int curr_file, int cmp_flag, int* occurrence) {
+int check_files(struct process* processes, int num_process, int curr_process, 
+        int curr_file, int cmp_flag, int* count) {
+    /* make sure current process is not outside the range*/
     assert((curr_process < num_process) && (curr_process >= 0));
+
     int j = curr_process - 1;
+    /* default for no match, otherwise returns the lowest matching process id */
+    int ret = -1; 
+
+    /* if count is not required to keep track of then just use dummy number */
+    int k = 0;
+    if (count != NULL) {
+        k = *count;
+    }
 
     switch (cmp_flag) {
         case CMP_LOCKED:
             while (j >= 0) {
-                if ((curr_file == processes[j].locked_file_id) && (occurrence == NULL)) { 
-                    return 0;
-                } else if (curr_file == processes[j].locked_file_id) {
-                    (*occurrence)++;
+                if (curr_file == processes[j].locked_file_id) {
+                    k++;
+                    ret = j; // or should i use ==> if ((ret > -1) && (j < ret)) { ret = j; } ... because we want the lowest id
                 }
                 j--;
             }
             break;
         case CMP_REQUESTED:
             while (j >= 0) {
-                if ((curr_file == processes[j].requested_file_id) && (occurrence == NULL)) { 
-                    return 0;
-                } else if (curr_file == processes[j].requested_file_id) {
-                    (*occurrence)++;
+                if (curr_file == processes[j].requested_file_id) {
+                    k++;
+                    ret = j;
                 }
                 j--;
             }
             break;
         case CMP_BOTH:
             while (j >= 0) {
-                if (((curr_file == processes[j].locked_file_id) || 
-                        (curr_file == processes[j].requested_file_id)) && 
-                        (occurrence == NULL)) { 
-                    return 0; 
-                } else if ((curr_file == processes[j].locked_file_id) || 
+                if ((curr_file == processes[j].locked_file_id) || 
                         (curr_file == processes[j].requested_file_id)) {
-                    (*occurrence)++;
+                    k++;
+                    ret = j;
                 }
                 j--;
             }
             break;
-        default:
-            printf("please enter flag value between 0 to 2");
-            break;
     }
-    /* no match */
-    return 1;
+
+    if (count != NULL) { *count = k; }
+
+    return ret;
 }
