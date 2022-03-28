a8681ef1d1815494a53ec67cfdd288af1a3f0c96
Brian Aung <soekhantaung16@gmail.com>
2022-03-28 22:56:59 +1100

    implement deadlock detection and breaking

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <stdbool.h>
 
+#include "rag.h"
+#include "dfs.h"
 #include "utils.h"
 
 int main(int argc, char **argv) {
@@ -39,10 +41,10 @@ int main(int argc, char **argv) {
         }
 
         /* it is a new file if it doesn't match with files from previous processes */
-        if (check_files(processes, num_processes, i, processes[i].locked_file_id, 2, NULL) == -1) {
+        if (check_files(processes, num_processes, i, processes[i].locked_file_id, 2, NULL)) {
             num_files++;
         }
-        if (check_files(processes, num_processes, i, processes[i].requested_file_id, 2, NULL) == -1) {
+        if (check_files(processes, num_processes, i, processes[i].requested_file_id, 2, NULL)) {
             num_files++;
         }
         /***************************************************************************/
@@ -62,7 +64,7 @@ int main(int argc, char **argv) {
                 int count = 1;
                 /* if the file is not the new file */
                 if (check_files(processes, num_processes, i, 
-                            processes[i].requested_file_id, 1, &count) != -1) {
+                            processes[i].requested_file_id, 1, &count)) {
                     /* then check if it has the highest occurrence */
                     if (count > highest_occurrence) { highest_occurrence = count; }
                 }
@@ -73,23 +75,40 @@ int main(int argc, char **argv) {
     }
 
     /* TASK 3 */
-    int to_break[num_processes];
-    int to_break_count = 0;
-    int tmp = 0;
-    for (int i=0; i<num_processes; i++) {
-        tmp = check_files(processes, num_processes, i, processes[i].requested_file_id, 0, NULL); 
-        if (tmp != -1) { to_break[to_break_count] = tmp; to_break_count++; }
+    int visited_count = 0;
+    struct node visited[num_processes + num_files];
+    for (int i=0; i<(num_processes + num_files); i++) {
+        visited[i].vertex = 0;
+        visited[i].type = 0;
     }
 
-    if (to_break_count) {
-        printf("Deadlock detected\nTerminate ");
-        for (int i=0; i<to_break_count; i++) {
-            printf("%d ", to_break[i]);
+    int adj_size = num_processes * 2;
+    struct graph* rag = init_graph(adj_size);
+    for (int i=0, j=0; i<num_processes; i++, j+=2) {
+        add_edge(rag, j, processes[i].process_id, 'p', processes[i].requested_file_id, 'r'); 
+        add_edge(rag, j+1, processes[i].locked_file_id, 'r', processes[i].process_id, 'p'); 
+    }
+
+    int to_terminate[num_processes];
+    int terminate_count = 0;
+    while (get_unvisited(rag, visited, &visited_count) != -1) {
+        int k = get_unvisited(rag, visited, &visited_count);
+        int smallest_process = visit_dfs(rag, visited, &visited_count, adj_size, k);
+        if (smallest_process != -1) {
+            to_terminate[terminate_count] = smallest_process;
+            terminate_count++;
+        } 
+    }
+
+    if (terminate_count) {
+        printf("Deadlock detected\nTerminate");
+        for (int i=0; i<terminate_count; i++) {
+            printf(" %d", to_terminate[i]);
         }
         printf("\n");
     } else {
         printf("No deadlocks\n");
     }
-
+    
     return 0;
 }
